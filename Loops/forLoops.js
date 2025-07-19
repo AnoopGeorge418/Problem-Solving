d6e9eac88fffe664eb014c073d8b4e5149ef1be6
Anoop George <anoopgeorge418@gmail.com>
2025-07-19 22:36:50 +0530

    10 Regular For Loops Problems

---
+++ Loops/forLoops.js
@@ -0,0 +1,84 @@
+// Q1. Print numbers from 1 to 10 using a for loop.
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);  
+// }
+
+// Q2. Print even numbers from 2 to 20.
+// for (let i = 2; i <= 20; i++){
+//     if (i%2 == 0){
+//         console.log(`${i} is even number`);
+//     } else {
+//         continue;
+//     }
+// }
+
+// Q3. Print numbers from 10 to 1 (in reverse).
+// for (let i = 10; i >= 1; i--) {
+//     console.log(i);
+// }
+
+// Q4. Print the square of numbers from 1 to 5.
+// for (let i=1; i<= 5; i++) {
+//     console.log(`Square of ${i} is ${i**2}`);
+// }
+
+// Q5. Given an array of fruits, print each fruit.
+// let fruits = ["apple", "banana", "cherry", "mango"];
+// for (let i = 0; i < fruits.length; i++) {
+//     console.log(fruits[i]);
+// }
+
+// Q6. Calculate the sum of numbers from 1 to 100 using a loop.
+// let sum = 0 ;
+// const number = 100;
+// for (let i = 1; i <= number; i++){
+//     sum += i;
+// }
+
+// console.log(sum);
+// Q7. Multiply all numbers in this array and print the result: 
+// let nums = [2, 3, 4];
+// output = 1;
+
+// for (let i = 0; i < nums.length; i++){
+//     output *= nums[i];
+// }
+
+// console.log(output);
+
+// Q8. Find the max number in an array using a for loop:
+// let nums = [10, 23, 3, 45, 7];
+// let maxNum = nums[0];
+
+// for (let i = 1; i < nums.length; i++){
+//     if (nums[i] > maxNum){
+//         maxNum = nums[i]
+//     }
+    
+// }
+
+// console.log(maxNum);
+
+
+// Q9. Count how many times the letter a appears in this string:
+// let str = "javascript is amazing and bananas";
+// let totalCount = 0;
+
+// for (let i = 0; i<str.length; i++) {
+//     if (str[i] === 'a') {
+//         totalCount++;
+//     }
+// }
+
+// console.log(totalCount);
+
+// Q10. Print a right-angle triangle pattern like this:
+// for (let i=0; i <= 5; i++) {
+//     let stars = '';
+//     for (let j=0; j<=i; j++){
+//         stars += '*';
+//     }
+//     console.log(stars);
+    
+// }
